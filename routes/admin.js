ff2f53927c6b77bc2de3c866cc9fead8dc00ffc2
BaoCQSE62960 <kungfuhakata@gmail.com>
2023-02-09 07:07:49 +0700

    update_list

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,21 @@ const router = Router();
 const pool = require('../db');
 
 //GET Role list
+router.get('/role/', async (req, res) => {
+    try {
+      const list =
+        await pool.query(`SELECT id, name
+       FROM role
+       ORDER BY id
+       DESC
+       ;
+      `);
+      res.status(200).json({ list: list.rows });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ msg: 'Lỗi hệ thống!' });
+    }
+  });
 
 //GET Personal transaction list
 
--- routes/major.js
+++ routes/major.js
@@ -3,6 +3,21 @@ const router = Router();
 const pool = require('../db');
 
 //GET Major list
+router.get('/', async (req, res) => {
+    try {
+      const list =
+        await pool.query(`SELECT id, name, status
+       FROM major
+       ORDER BY id
+       DESC
+       ;
+      `);
+      res.status(200).json({ list: list.rows });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ msg: 'Lỗi hệ thống!' });
+    }
+  });
 
 //Add major
 
